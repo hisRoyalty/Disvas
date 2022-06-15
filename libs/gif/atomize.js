f94f399187298b61a52bb697cb4ca92e115104c0
hisRoyalty <hisroyal.baguette@gmail.com>
2022-06-15 20:59:04 +0800

    🎨 Quick changes Wooh!

--- libs/gif/atomize.js
+++ libs/gif/atomize.js
@@ -1,129 +1,126 @@
-const GIFEncoder = require('gifencoder');
-const Canvas = require('canvas');
+const GIFEncoder = require("gifencoder");
+const Canvas = require("canvas");
 
 module.exports = async (image) => {
-	const img = await Canvas.loadImage(image);
-	const encoder = new GIFEncoder(280, 280);
-	encoder.start();
-	encoder.setRepeat(0);
-	encoder.setDelay(0);
-	encoder.setQuality(2);
-	const canvas = Canvas.createCanvas(280, 280);
-	const ctx = canvas.getContext('2d');
-	ctx.drawImage(img, 0, 0, 280, 280);
-	const pixels = ctx.getImageData(0, 0, 280, 280);
-	ctx.clearRect(0, 0, 280, 280);
-	const particlesArray = [];
-	function generateRandom(min = 6000, max = 6280) {
-		const diff = max - min;
-		let rand = Math.random();
-		rand = Math.floor(rand * diff);
-		return rand + min;
-	}
-	const numberOfParticles = generateRandom();
-	ctx.fillText();
-	const mappedImage = [];
+  const img = await Canvas.loadImage(image);
+  const encoder = new GIFEncoder(280, 280);
+  encoder.start();
+  encoder.setRepeat(0);
+  encoder.setDelay(0);
+  encoder.setQuality(2);
+  const canvas = Canvas.createCanvas(280, 280);
+  const ctx = canvas.getContext("2d");
+  ctx.drawImage(img, 0, 0, 280, 280);
+  const pixels = ctx.getImageData(0, 0, 280, 280);
+  ctx.clearRect(0, 0, 280, 280);
+  const particlesArray = [];
+  function generateRandom(min = 6000, max = 6280) {
+    const diff = max - min;
+    let rand = Math.random();
+    rand = Math.floor(rand * diff);
+    return rand + min;
+  }
+  const numberOfParticles = generateRandom();
+  ctx.fillText();
+  const mappedImage = [];
 
-	for (let y = 0; y < 280; y++) {
-		const row = [];
-		for (let x = 0; x < 280; x++) {
-			const red = pixels.data[y * 4 * pixels.width + x * 4];
-			const green = pixels.data[y * 4 * pixels.width + (x * 4 + 1)];
-			const blue = pixels.data[y * 4 * pixels.width + (x * 4 + 2)];
-			const brightness = calculateRelativeBrightness(red, green, blue);
-			let cellBrightness = undefined;
-			let cellColor = undefined;
-			const cell = [
-				(cellBrightness = brightness),
-				(cellColor = `rgb(${red},${green},${blue})`),
-			];
-			row.push(cell);
-		}
-		mappedImage.push(row);
-	}
-		
+  for (let y = 0; y < 280; y++) {
+    const row = [];
+    for (let x = 0; x < 280; x++) {
+      const red = pixels.data[y * 4 * pixels.width + x * 4];
+      const green = pixels.data[y * 4 * pixels.width + (x * 4 + 1)];
+      const blue = pixels.data[y * 4 * pixels.width + (x * 4 + 2)];
+      const brightness = calculateRelativeBrightness(red, green, blue);
+      let cellBrightness = undefined;
+      let cellColor = undefined;
+      const cell = [
+        (cellBrightness = brightness),
+        (cellColor = `rgb(${red},${green},${blue})`),
+      ];
+      row.push(cell);
+    }
+    mappedImage.push(row);
+  }
 
-	// formula for calculating the brightness of a pixel
-	function calculateRelativeBrightness(red, green, blue) {
-		return (
-			Math.sqrt(
-				red * red * 0.299 + green * green * 0.587 + blue * blue * 0.114
-			) / 280
-		);
-	}
+  // formula for calculating the brightness of a pixel
+  function calculateRelativeBrightness(red, green, blue) {
+    return (
+      Math.sqrt(
+        red * red * 0.299 + green * green * 0.587 + blue * blue * 0.114
+      ) / 280
+    );
+  }
 
-	/*
-				* @INFO
-				* function to calculate the brightness of a pixel
-				https://stackoverflow.com/questions/596216/formula-to-determine-perceived-brightness-of-rgb-color
-				check this link if you have problems.
-				*/
-	const Particle = (() => {
-		function Particle() {
-			this.x = Math.random() * 280;
-			this.y = Math.random() * 280;
-			this.speed = 0;
-			this.velocity = Math.random() * 0.5;
-			this.size = Math.random() * 2.5 + 0.2;
-			this.position1 = Math.floor(this.y);
-			this.position2 = Math.floor(this.x);
-			this.angle = 0;
-			this.random = Math.random();
-		}
-				Particle.prototype.update = function () {
-						this.position1 = Math.floor(this.y);
-						this.position2 = Math.floor(this.x);
-						if (
-								mappedImage[this.position1]?.[this.position2]
-						) {
-								this.speed = mappedImage[this.position1][this.position2][0];
-						}
-					const movement = 2.5 - this.speed + this.velocity;
-						this.angle += this.speed / 20;
-						this.size = this.speed * 2.8;
-						this.y -= movement;
-						this.x += movement + Math.sin(this.angle) * 2;
-						if (this.y <= 0) {
-								this.y = 280;
-								this.x = Math.random() * 280;
-						}
-						if (this.x >= 280) {
-								this.x = 0;
-								this.y = Math.random() * 280;
-						}
-				};
-		Particle.prototype.draw = function () {
-			ctx.beginPath();
-			if (mappedImage[this.position1]?.[this.position2]) {
-				ctx.fillStyle = mappedImage[this.position1][this.position2][1];
-				ctx.strokeStyle = mappedImage[this.position1][this.position2][1];
-			}
-			ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
-			ctx.fill();
-		};
-		return Particle;
-	})();
-	function init() {
-		for (let i = 0; i < numberOfParticles; i++) {
-			particlesArray.push(new Particle());
-		}
-	}
-	init();
-	function animate() {
-		ctx.globalAlpha = 0.05;
-		ctx.fillStyle = "rgb(0, 0, 0)";
-		ctx.fillRect(0, 0, 280, 280);
-		ctx.globalAlpha = 0.2;
-		particlesArray.forEach((particlesArrayItem, i) => {
-				particlesArrayItem.update();
-				if(i % 10 == 0 && i > 5100) { 
-						encoder.addFrame(ctx);
-				}
-				ctx.globalAlpha = 1;
-				particlesArrayItem.draw();
-		});
-	}
+  /*
+   * @INFO
+   * function to calculate the brightness of a pixel
+   * https://stackoverflow.com/questions/596216/formula-to-determine-perceived-brightness-of-rgb-color
+   * Check this link if you have problems.
+   */
+  const Particle = (() => {
+    function Particle() {
+      this.x = Math.random() * 280;
+      this.y = Math.random() * 280;
+      this.speed = 0;
+      this.velocity = Math.random() * 0.5;
+      this.size = Math.random() * 2.5 + 0.2;
+      this.position1 = Math.floor(this.y);
+      this.position2 = Math.floor(this.x);
+      this.angle = 0;
+      this.random = Math.random();
+    }
+    Particle.prototype.update = function () {
+      this.position1 = Math.floor(this.y);
+      this.position2 = Math.floor(this.x);
+      if (mappedImage[this.position1]?.[this.position2]) {
+        this.speed = mappedImage[this.position1][this.position2][0];
+      }
+      const movement = 2.5 - this.speed + this.velocity;
+      this.angle += this.speed / 20;
+      this.size = this.speed * 2.8;
+      this.y -= movement;
+      this.x += movement + Math.sin(this.angle) * 2;
+      if (this.y <= 0) {
+        this.y = 280;
+        this.x = Math.random() * 280;
+      }
+      if (this.x >= 280) {
+        this.x = 0;
+        this.y = Math.random() * 280;
+      }
+    };
+    Particle.prototype.draw = function () {
+      ctx.beginPath();
+      if (mappedImage[this.position1]?.[this.position2]) {
+        ctx.fillStyle = mappedImage[this.position1][this.position2][1];
+        ctx.strokeStyle = mappedImage[this.position1][this.position2][1];
+      }
+      ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+      ctx.fill();
+    };
+    return Particle;
+  })();
+  function init() {
+    for (let i = 0; i < numberOfParticles; i++) {
+      particlesArray.push(new Particle());
+    }
+  }
+  init();
+  function animate() {
+    ctx.globalAlpha = 0.05;
+    ctx.fillStyle = "rgb(0, 0, 0)";
+    ctx.fillRect(0, 0, 280, 280);
+    ctx.globalAlpha = 0.2;
+    particlesArray.forEach((particlesArrayItem, i) => {
+      particlesArrayItem.update();
+      if (i % 10 == 0 && i > 5100) {
+        encoder.addFrame(ctx);
+      }
+      ctx.globalAlpha = 1;
+      particlesArrayItem.draw();
+    });
+  }
   animate();
-	encoder.finish();
-	return encoder.out.getData();
-}
+  encoder.finish();
+  return encoder.out.getData();
+};
--- libs/image/fakeMessage.js
+++ libs/image/fakeMessage.js
@@ -4,98 +4,96 @@
 
 const Canvas = require('canvas');
 
-module.exports = async(displayName, displayAvatar, roleColor, text) => {
-    let textArray = [];
-    let getStretch = textAlign(text, textArray);
-    text = textArray.map(t => `${t}`).join("\n");
-    
-    if(roleColor === '#000000') { 
-        roleColor = '#fdfdfc'
-    };
-   try {
-    // Register Font
-    Canvas.registerFont("./assets/fonts/whitneybook.otf", { family: "whitneybook" } );
-    Canvas.registerFont("./assets/fonts/whitneylight.otf", { family: "whitneylight" } );
-    Canvas.registerFont("./assets/fonts/whitneymedium.otf", { family: "whitneymedium" } );
-    // Creating Background
-    let stretch = 20 * getStretch;
-    const width = 700;
-    const height = 100 + stretch;
-    const canvas = Canvas.createCanvas(width, height);
-	const context = canvas.getContext('2d');
-    context.fillStyle = '#36393f'
-    context.fillRect(0, 0, width, height)
-    // Display Name
-    context.font = '20px whitneymedium';
-    context.fillStyle = `${roleColor}`;
-    context.fillText(`${displayName}`, 120, 45);
-    // Time
-    let time = new Date().toLocaleString('en-US', { hour: 'numeric', minute: "numeric", hour12: true });
-    let x = alignTime(displayName)
-    context.font = '15px whitneylight';
-    context.fillStyle = '#a3a6aa';
-    context.fillText(`Today at ${time}`, x, 45);
-    // Text
-    context.font = '18px whitneybook';
-    context.fillStyle = `#ffffff`;
-    context.fillText(`${text}`, 120, 75);
-    // User Avatar
-    context.beginPath();
-	context.arc(60, 60, 35, 0, Math.PI * 2, true);
-	context.closePath();
-	context.clip();
-    let avatar = await Canvas.loadImage(displayAvatar);
-	context.drawImage(avatar, 25, 25, 70, 70);
-    context.restore();
+module.exports = async (displayName, displayAvatar, roleColor, text) => {
+	const textArray = [];
+	const getStretch = textAlign(text, textArray);
+	text = textArray.map((t) => `${t}`).join('\n');
 
-    // return data
-    return canvas.toBuffer();
+	if (roleColor === '#000000') {
+		roleColor = '#fdfdfc';
+	}
+	
+		// Register Font
+		Canvas.registerFont('./assets/fonts/whitneybook.otf', {
+			family: 'whitneybook',
+		});
+		Canvas.registerFont('./assets/fonts/whitneylight.otf', {
+			family: 'whitneylight',
+		});
+		Canvas.registerFont('./assets/fonts/whitneymedium.otf', {
+			family: 'whitneymedium',
+		});
+		// Creating Background
+		const stretch = 20 * getStretch;
+		const width = 700;
+		const height = 100 + stretch;
+		const canvas = Canvas.createCanvas(width, height);
+		const context = canvas.getContext('2d');
+		context.fillStyle = '#36393f';
+		context.fillRect(0, 0, width, height);
+		// Display Name
+		context.font = '20px whitneymedium';
+		context.fillStyle = `${roleColor}`;
+		context.fillText(`${displayName}`, 120, 45);
+		// Time
+		const time = new Date().toLocaleString('en-US', {
+			hour: 'numeric',
+			minute: 'numeric',
+			hour12: true,
+		});
+		const x = alignTime(displayName);
+		context.font = '15px whitneylight';
+		context.fillStyle = '#a3a6aa';
+		context.fillText(`Today at ${time}`, x, 45);
+		// Text
+		context.font = '18px whitneybook';
+		context.fillStyle = '#ffffff';
+		context.fillText(`${text}`, 120, 75);
+		// User Avatar
+		context.beginPath();
+		context.arc(60, 60, 35, 0, Math.PI * 2, true);
+		context.closePath();
+		context.clip();
+		const avatar = await Canvas.loadImage(displayAvatar);
+		context.drawImage(avatar, 25, 25, 70, 70);
+		context.restore();
 
- } catch (e) {
-        console.log(e);
-    }
+		// return data
+		return canvas.toBuffer();
+	
 };
 
 // aligns Text
 function textAlign(text, array) {
-let align = text;
-let tries = text.length / 45;
-let split = text.split("\n");
-if(tries === 0) {
-    tries = 1;
-}
-if(tries != Math.floor(tries)) {
-    tries = Math.floor(tries) + 1;
-}
-for (let i = 0; i < tries; i++) {
-let toSub = 45;
-align.split("").forEach(t => {
-if(t.toUpperCase() == t) {
-    toSub = toSub
-} else {
-    toSub = toSub + 0.1
-}
-})
-let subsNum = Math.floor(toSub);
-console.log(subsNum)
-let push = align.substring(0, subsNum);
-align = align.slice(subsNum);
-array.push(push);
-  }
-  tries = tries + split.length
-  return tries;
+	let align = text;
+	let tries = text.length / 45;
+	const split = text.split('\n');
+	if (tries === 0) {
+		tries = 1;
+	}
+	if (tries != Math.floor(tries)) {
+		tries = Math.floor(tries) + 1;
+	}
+	for (let i = 0; i < tries; i++) {
+		let toSub = 45;
+		align.split('').forEach((t) => {
+			toSub = t.toUpperCase() == t ? toSub : toSub + 0.1;
+		});
+		const subsNum = Math.floor(toSub);
+		const push = align.substring(0, subsNum);
+		align = align.slice(subsNum);
+		array.push(push);
+	}
+	tries += split.length;
+	return tries;
 }
 
 // align Time
 function alignTime(name) {
-let x = 120;
-characters = name.split("")
-characters.forEach(c => {
-    if(c.toUpperCase() == c) {
-    x = x + 14.6
-    } else {
-    x = x + 10.6
-    }
-  })
-    return x;
+	let x = 120;
+	characters = name.split('');
+	characters.forEach((c) => {
+		x = c.toUpperCase() == c ? x + 14.6 : x + 10.6;
+	});
+	return x;
 }
