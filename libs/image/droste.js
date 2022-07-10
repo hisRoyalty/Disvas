cf9f1f80ac3a0c7a03bc0ac17fe5239a1af0d3ee
hisRoyalty <hisroyal.baguette@gmail.com>
2022-07-10 17:50:02 +0800

    ♿️ Improvise droste effect +1 to accessibility.

--- libs/image/droste.js
+++ libs/image/droste.js
@@ -1,26 +1,19 @@
-const Canvas = require('canvas')
+const Canvas = require('canvas');
 
+module.exports = async (image) => {
+	const img = await Canvas.loadImage(image);
+	const w = 800;
+	const h = 600;
+	const canvas = Canvas.createCanvas(w, h);
+	const ctx = canvas.getContext('2d');
 
+	//TODO: Improve code, write better code and not so ugly code 💩
+	ctx.drawImage(img, 0, 0, w, h);
+	ctx.drawImage(img, 30, 55, w - 60, h - 110);
+	ctx.drawImage(img, 80, 100, w - 80 * 2, h - 100 * 2);
+	ctx.drawImage(img, 110, 150, w - 110 * 2, h - 150 * 2);
+	ctx.drawImage(img, 130, 185, w - 130 * 2, h - 185 * 2);
+	ctx.drawImage(img, 150, 220, w - 150 * 2, h - 220 * 2);
 
-
-
-module.exports = async(image) => {
-    const img = await Canvas.loadImage(image)
-    let w = 800
-    let h = 600
-    const canvas = Canvas.createCanvas(w, h)
-    const ctx = canvas.getContext("2d")
-    
-    //TODO: Improve code, write better code and not so ugly code 💩
-    ctx.drawImage(img, 0, 0, w, h)
-    ctx.drawImage(img, 30, 55, w-60, h-110)
-    ctx.drawImage(img, 80, 100, w-80*2, h-100*2)
-    ctx.drawImage(img, 110, 150, w-110*2, h-150*2)
-    ctx.drawImage(img, 130, 185, w-130*2, h-185*2)
-    ctx.drawImage(img, 150, 220, w-150*2, h-220*2)
-    
-
-    
-    return canvas.toBuffer()
-
-}
+	return canvas.toBuffer();
+};
